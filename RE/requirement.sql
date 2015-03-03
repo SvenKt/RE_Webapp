0e2b1fabde8889b1ceb44a70346ddd369f66120d
Sven <sv-kujat@t-online.de>
2015-03-03 14:17:37 +0100

    aktueller mysql dump

--- RE/requirement.sql
+++ RE/requirement.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 16. Feb 2015 um 15:52
+-- Erstellungszeit: 03. Mrz 2015 um 14:16
 -- Server Version: 5.6.21
 -- PHP-Version: 5.6.3
 
@@ -28,16 +28,38 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `requirements` (
 `id` int(11) unsigned NOT NULL,
-  `requirement` varchar(200) DEFAULT NULL,
-  `owner_id` int(6) unsigned DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `requirement` varchar(200) CHARACTER SET utf8 COLLATE utf8_german2_ci DEFAULT NULL,
+  `owner_id` int(11) unsigned DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `requirements`
 --
 
 INSERT INTO `requirements` (`id`, `requirement`, `owner_id`) VALUES
-(1, 'Das System muss dem Nutzer die Möglichkeit bieten, sich einzuloggen', 1);
+(1, 'Das System muss dem Nutzer die Möglichkeit bieten, sich einzuloggen', 1),
+(2, 'ich muss muss essen fÃ¤hig sein und du.', 1),
+(3, 'ich muss muss essen fÃ¤hig sein und du.', 1),
+(4, 'Ã¶oij muss pij pij fÃ¤hig sein pj pij.', 1),
+(5, 'd muss d fÃ¤hig sein d d.', 1),
+(6, 'd muss 3 fÃ¤hig sein d d.', 1),
+(9, 'bla muss abl abl fÃ¤hig sein bla bla.', 1),
+(13, 'asx muss asx as fÃ¤hig sein asx asx.', 4),
+(16, 'aa:muss:aa:a :fÃ¤hig sein :a:a:.', 5),
+(19, 'ich:muss:ih:ioh  :fÃ¤hig sein :iu:iuh:.', 5),
+(21, 'ihc:muss:iuh:iuh  :fÃ¤hig sein :piuh:piuh:.', 6),
+(22, 'bla:muss:bla:bl :fÃ¤hig sein :bli:blubb:.', 6),
+(23, 'ss:muss:guz:oz :fÃ¤hig sein :gzou:gz:.', 6),
+(24, 'utzfguztfg:muss:fiz:tfizt :fÃ¤hig sein :fzitf:zitf:.', 6),
+(25, 'iuhsdoiuh:muss:oiuhoiuh:oiuhoipuh :fÃ¤hig sein :piuopiuh:piuhpiuh:.', 6),
+(26, 'kluh:muss:liuh:luih :fÃ¤hig sein :liuh:luh:.', 6),
+(27, 'iu:muss:oiuhtrd::fÃ¤hig sein :zter:zers:.', 6),
+(28, '645:muss:64:64 :fÃ¤hig sein :654654:4:.', 6),
+(29, 'uzkg:muss:oguz:ozg :fÃ¤hig sein :ozg:ogz:.', 6),
+(30, 'uzg:muss:uiogz:iozug :fÃ¤hig sein :izug:iguz:.', 6),
+(31, '75:muss:izgu:gzi :fÃ¤hig sein :uzg:ugz:.', 6),
+(32, 'pui:muss:iu:oiuh :fÃ¤hig sein :oiuhoiu:hoiuh:.', 6),
+(33, 'asdoijaopdijaspodijapsodijasopdijaopsidjaposidjaspoj:muss:opuijpouijpoijpoijpojipoijpojpojipoijpoij:pouhuzhb8zjoiuzuigztjtrhgjf5zgrfzhdt4rf4 :fÃ¤hig sein :fuzrttzfgzrzh5rgugztrzth:ttugrtzrh65uggrghzrt', 6);
 
 -- --------------------------------------------------------
 
@@ -46,41 +68,23 @@ INSERT INTO `requirements` (`id`, `requirement`, `owner_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-`id` int(6) unsigned NOT NULL,
+`id` int(11) unsigned NOT NULL,
   `username` varchar(30) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `email` varchar(60) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'sven', 'a8efc104ad9d5ba4e3bcbda231d2e042', 'neu@t-online'),
-(2, 'test2', '827ccb0eea8a706c4c34a16891f84e7b', 'sven@ich'),
-(3, 'test2', 'c20ad4d76fe97759aa27a0c99bff6710', 'sven@ich'),
-(4, 'terik', '202cb962ac59075b964b07152d234b70', 'sven@ich'),
-(5, 'test2', '202cb962ac59075b964b07152d234b70', 'sven@ich'),
-(6, '', 'd41d8cd98f00b204e9800998ecf8427e', ''),
-(7, 'neu', '83e71d83b2dcea23dcab3b64182494cc', 'neu'),
-(8, 'neu2', '83e71d83b2dcea23dcab3b64182494cc', 'neu'),
-(9, '123', '202cb962ac59075b964b07152d234b70', '123'),
-(10, 'tom', '94846ab27a4abb8672589ca403d0dc43', 'sven'),
-(11, 'thomas', '94846ab27a4abb8672589ca403d0dc43', '123'),
-(12, 'marvin', '202cb962ac59075b964b07152d234b70', 'meineemail'),
-(13, 'marvin94', 'f40790f3d45d9e181d39cbdbaf2bf030', 'kekse'),
-(14, 'hallo', '598d4c200461b81522a3328565c25f7c', 'hallo'),
-(15, 'hallo2', 'c4ca4238a0b923820dcc509a6f75849b', 'hallo'),
-(16, 'hallo3', 'c4ca4238a0b923820dcc509a6f75849b', 'hallo'),
-(17, 'hallo5', 'c81e728d9d4c2f636f067f89cc14862c', 'hallo'),
-(18, 'sven-e', '6ea41f662929e80ec3595fa51c26940d', '12'),
-(19, 'hammel', '25d55ad283aa400af464c76d713c07ad', '123'),
-(20, 'nana', '185aef3b1c810799a6be8314abf6512c', 'bla'),
-(21, 'mama', '25d55ad283aa400af464c76d713c07ad', '34'),
-(22, 'LANG', '', '147'),
-(23, 'lala', 'e40f01afbb1b9ae3dd6747ced5bca532', '147'),
-(24, 'fredy', '11e085377b4a3c5efbfba3d09698a484', 'sv-kujat@t-online.de');
+(1, 'sven', '81dc9bdb52d04dc20036dbd8313ed055', NULL),
+(2, 'test', 'test', NULL),
+(3, 'SvenKt', 'c3fbcd8f3855a7050a359679effbbdf4', 'sv-kujat@t-online.de'),
+(4, 'peter', '25d55ad283aa400af464c76d713c07ad', '123'),
+(5, 'ich', '25d55ad283aa400af464c76d713c07ad', 'ich_ich@t-online.de'),
+(6, 'admin', 'c3fbcd8f3855a7050a359679effbbdf4', 'ich@admin.de');
 
 --
 -- Indizes der exportierten Tabellen
@@ -90,7 +94,7 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
 -- Indizes für die Tabelle `requirements`
 --
 ALTER TABLE `requirements`
- ADD PRIMARY KEY (`id`), ADD KEY `owner_id` (`owner_id`);
+ ADD PRIMARY KEY (`id`), ADD KEY `fk_owner` (`owner_id`);
 
 --
 -- Indizes für die Tabelle `users`
@@ -106,12 +110,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT für Tabelle `requirements`
 --
 ALTER TABLE `requirements`
-MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=34;
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(6) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+MODIFY `id` int(11) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- Constraints der exportierten Tabellen
 --
@@ -120,7 +124,7 @@ MODIFY `id` int(6) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
 -- Constraints der Tabelle `requirements`
 --
 ALTER TABLE `requirements`
-ADD CONSTRAINT `requirements_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
+ADD CONSTRAINT `fk_owner` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
