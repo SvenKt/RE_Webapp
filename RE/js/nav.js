469e0c18110454674eca1dd4f6c3885ede3982fc
Marvin Hartmann <Marvin.Hartmann@msasafety.com>
2015-04-08 14:54:39 +0200

    delete console.log from nav.js and fixed a css fail

--- RE/js/nav.js
+++ RE/js/nav.js
@@ -2,7 +2,6 @@ angular
 	.module("requirements", [])
 	//Alles ab hier controller
 	.controller("NavCtrl", function($scope) {
-	console.log("clicked");
 	
 		$('#main-nav li a').on('click', function() {
 		$(this).parent().parent().find('.active').removeClass('active navlist');
